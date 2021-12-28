a8fd809544ff4a677323da23c9ea8514867eb55d
Abdul Awal Nadim <nadim.ice.nstu@gmail.com>
2021-12-28 22:08:56 +0600

    computer graphics' algorithm practices

---
+++ GarbageValue.cpp
@@ -0,0 +1,33 @@
+///................................Nadim.................................///
+///..............Noakhali Science and Technology University..............///
+///........Department of Information and Communication Engineering.......///
+#include<bits/stdc++.h>
+#include<iostream>
+#include<climits>
+using namespace std;
+#define    IOS            ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define    MX             1000000000
+#define    fill(a,b)      memset(a,b,sizeof(a))
+#define    gcd(a,b)     __gcd(a,b)
+#define    lcm(a,b)       (a*(b/gcd(a,b)))
+#define    ll             long long int
+
+int main(){
+    //ll test;cin>>test;while(test--)
+        {
+
+            int i=5,j=5;
+            cout<<"i = "<<i<<" j = "<<j<<endl;
+
+            cout<<"++i = "<<++i<<" ++j = "<<++j<<endl;
+            cout<<"After ++i and ++j"<<endl;
+            cout<<"i = "<<i<<" j = "<<j<<endl<<endl;
+
+            int a=5,b=5;
+            cout<<"a = "<<a<<" b = "<<b<<endl;
+            cout<<"a++ = "<<a++<<" b++ = "<<b++<<endl;
+            cout<<"After a++ and b++"<<endl;
+            cout<<"a = "<<a<<" b = "<<b<<endl;
+        }
+return 0;
+}
